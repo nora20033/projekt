682697fc5142902fb4d9513cc85f07cc4762033f
Nora Šundov <sundovnora@gmail.com>
2020-06-15 12:59:56 +0200

    Updates

---
+++ precache-manifest.3a1ba567f75358573a99494f3d411f92.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d3cdf912d80a4f427b0c20475f5db9",
+    "url": "/nora_jsrs/index.html"
+  },
+  {
+    "revision": "8c94e518e39c9bfc1f6d",
+    "url": "/nora_jsrs/static/css/main.0787853d.chunk.css"
+  },
+  {
+    "revision": "4e73dd3b8d0c62038edb",
+    "url": "/nora_jsrs/static/js/2.9b2fad85.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/nora_jsrs/static/js/2.9b2fad85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c94e518e39c9bfc1f6d",
+    "url": "/nora_jsrs/static/js/main.0d6c8e5e.chunk.js"
+  },
+  {
+    "revision": "c3db69582ad389f35720",
+    "url": "/nora_jsrs/static/js/runtime-main.bf66b409.js"
+  },
+  {
+    "revision": "56fd6323f9d17f710fb673157e7daaae",
+    "url": "/nora_jsrs/static/media/druga.56fd6323.jpg"
+  },
+  {
+    "revision": "c30b8b296c0890e98a7e9c9d7eaef9d6",
+    "url": "/nora_jsrs/static/media/haii.c30b8b29.jpg"
+  },
+  {
+    "revision": "2fbbe632e81ccff66e46ac4941a763e8",
+    "url": "/nora_jsrs/static/media/jej.2fbbe632.jpg"
+  },
+  {
+    "revision": "6af10df65ecbc7bb3a03392ecf2050d4",
+    "url": "/nora_jsrs/static/media/mljett.6af10df6.jpg"
+  },
+  {
+    "revision": "d4cfbd9e262c3e138d5cf8b75bb86ed7",
+    "url": "/nora_jsrs/static/media/treca.d4cfbd9e.jpg"
+  },
+  {
+    "revision": "0318a80f0e47cb7913fed8501a0f618a",
+    "url": "/nora_jsrs/static/media/zadarr.0318a80f.jpg"
+  }
+]);
